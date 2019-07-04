26ea0b40acd535cb6b506c3b1a8116bf6a7830eb
Lisa Carbonell <morekaleplease@gmail.com>
2019-07-03 22:25:10 -0700

    Courses.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import axios from "axios";
-
+import "./css/global.css";
 // import components
 import CourseDetail from './components/CourseDetail';
 import Courses from './components/Courses';
